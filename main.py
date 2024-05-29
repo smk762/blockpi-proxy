aed382ebcd7513f458d3aa66218e42d1add9e36a
smk762 <smk@komodoplatform.com>
2024-05-29 11:54:45 +0800

    send error msg if network unsupported

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect, APIRouter
 from starlette.requests import Request
 from starlette.responses import StreamingResponse
 from starlette.background import BackgroundTask
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse
 import httpx  # httpx streaming allows for larger files & less memory usage
 from websockets import connect
 from logger import logger
@@ -31,16 +31,6 @@ if config.FASTAPI["USE_MIDDLEWARE"]:
     )
 
 
-@app.get("/", tags=["api"])
-def get_response(request: Request):
-    return {"info": "go to /docs for API documentation"}
-
-
-@app.get("/api/v1/healthcheck", tags=["api"])
-def get_response(request: Request):
-    return {"status": "online"}
-
-
 async def get_rpc_resp(network, path):
     client = httpx.AsyncClient(base_url=config.API_URLS[network]["rpc"])
     req = client.build_request("GET", path)
@@ -50,30 +40,53 @@ async def get_rpc_resp(network, path):
     )
 
 
-@app.api_route(
-    "/rpc/{network}/{path:path}",
-    methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH"],
-)
-async def get_rpc(network: str, path: str):
-    return await get_rpc_resp(network, path)
-
-
 async def get_ws_resp(websocket: WebSocket, network):
     await websocket.accept()
-    upstream_ws_url = config.API_URLS[network]["wss"]
-    logger.calc(upstream_ws_url)
+    if network not in config.API_URLS:
+        upstream_ws_url = "wss://echo.websocket.org"
+    else:
+        upstream_ws_url = config.API_URLS[network]["wss"]
     async with connect(upstream_ws_url) as upstream_ws:
         try:
             while True:
-                data = await websocket.receive_text()
-                await upstream_ws.send(data)
-                response = await upstream_ws.recv()
-                await websocket.send_text(response)
+                if network not in config.API_URLS:
+                    await websocket.send_text(str({"error": f"network {network} not supported!"}))
+                    raise WebSocketDisconnect
+                else:
+                    data = await websocket.receive_text()
+                    await upstream_ws.send(data)
+                    response = await upstream_ws.recv()
+                    await websocket.send_text(response)
         except WebSocketDisconnect:
             await websocket.close()
         except Exception as e:
             await websocket.close()
             raise e
+
+
+
+@app.get("/")
+def welcome(request: Request):
+    return {
+        "message": "Welcome to the Blockpi proxy API!",
+        "supported_networks": list(config.API_URLS.keys()),
+        "info": "See /docs for API documentation"
+    }
+
+
+@app.get("/api/v1/healthcheck")
+def healthcheck(request: Request):
+    return {"status": "online"}
+
+
+@app.api_route(
+    "/rpc/{network}/{path:path}",
+    methods=["GET", "POST", "OPTIONS"],
+)
+async def get_rpc(network: str, path: str):
+    if network not in config.API_URLS:
+        return JSONResponse({"error": f"network {network} not supported!"})
+    return await get_rpc_resp(network, path)
 
 
 @app.websocket("/ws/{network}")
